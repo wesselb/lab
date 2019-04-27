34e5961b073bb14819024494bf1e9ab1515629fe
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-04-27 23:52:07 +0100

    Clean tests

--- tests/test_linear_algebra.py
+++ tests/test_linear_algebra.py
@@ -6,5 +6,5 @@ from __future__ import absolute_import, division, print_function
 from . import eq, neq, lt, le, ge, gt, raises, call, ok, lam, allclose, approx
 
 
-def test():
-    yield eq, 1, 1
+def test_matmul():
+    raise NotImplementedError
--- tests/util.py
+++ tests/util.py
@@ -2,8 +2,6 @@
 
 from __future__ import absolute_import, division, print_function
 
-import sys
-
 from nose.tools import assert_raises, assert_equal, assert_less, \
     assert_less_equal, assert_not_equal, assert_greater, \
     assert_greater_equal, ok_
@@ -27,7 +25,3 @@ def call(f, method, args=(), res=True):
 
 def lam(f, args=()):
     ok_(f(*args), 'Lambda returned False.')
-
-
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
