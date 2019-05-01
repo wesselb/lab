dd9492b4f4a8b104aa5f2cbebc22147c9f65286a
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-05-01 23:14:59 +0100

    Refactor abstract

--- lab/util.py
+++ lab/util.py
@@ -4,31 +4,20 @@ from __future__ import absolute_import, division, print_function
 
 from functools import wraps
 
-from plum import convert, promote
+import plum
 
-from .types import Framework
 from . import B
 
 __all__ = ['abstract']
 
 
-def _determine_splitting_index(args, to):
-    if to is None:
-        return 0
-    elif to < 0:
-        return to + len(args) + 1
-    else:
-        return to + 1
-
-
-def abstract(convert_to=-1, promote_to=-1):
+def abstract(promote=-1):
     """Create a decorator for an abstract function.
 
     Args:
-        convert_to (int, optional): Index to which to convert arguments. Set to
-            `None` to convert no arguments. Defaults to `-1`.
-        promote_to (int, optional): Index to which to promote arguments. Set to
-            `None` to promote no arguments. Defaults to `-1`.
+        promote (int, optional): Number of arguments to promote. Set to `-1`
+            to promote all arguments, and set to `None` or `0` to promote no
+            arguments. Defaults to `-1`.
 
     Returns:
         function: Decorator.
@@ -37,24 +26,29 @@ def abstract(convert_to=-1, promote_to=-1):
     def decorator(f):
         @wraps(f)
         def wrapper(*args, **kw_args):
-            # Determine splitting indices.
-            convert_index = _determine_splitting_index(args, convert_to)
-            promote_index = _determine_splitting_index(args, promote_to)
+            # Determine splitting index.
+            if promote is None or promote == 0:
+                promote_index = 0
+            elif promote < 0:
+                promote_index = len(args) + 1
+            else:
+                promote_index = promote
 
             # Record types.
-            types_before = tuple(type(arg) for arg in args)
+            types_before = tuple(plum.type_of(arg) for arg in args)
 
-            # Convert and promote.
-            args = tuple(convert(x, Framework)
-                         for x in args[:convert_index]) + args[convert_index:]
-            args = promote(*args[:promote_index]) + args[promote_index:]
+            # Promote.
+            args = plum.promote(*args[:promote_index]) + args[promote_index:]
 
-            # Enforce a change in types. Otherwise, the call will recurse.
-            types_after = tuple(type(arg) for arg in args)
+            # Enforce a change in types. Otherwise, the call will recurse, which
+            # means that an implementation is not available.
+            types_after = tuple(plum.type_of(arg) for arg in args)
             if types_before == types_after:
-                raise NotImplementedError(
-                    'No implementation of "{}" for argument(s) {}.'
-                    ''.format(f.__name__, args))
+                signature = plum.Tuple(*types_after)
+                # TODO: Use the message from Plum directly here.
+                raise plum.NotFoundLookupError(
+                    'For function "{}", signature {} could not be resolved.'
+                    ''.format(f.__name__, signature))
 
             # Retry call.
             return getattr(B, f.__name__)(*args, **kw_args)
