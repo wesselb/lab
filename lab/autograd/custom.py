Rename numpy to autograd, fix gradient bug, add bvn_cdf

lab/autograd/custom.py
@@ -4,8 +4,28 @@ from __future__ import absolute_import, division, print_function
 
 import numpy as np
 from autograd.extend import primitive, defvjp_argnums
+from plum import Dispatcher
 
 __all__ = ['autograd_register']
+_dispatch = Dispatcher()
+
+
+@_dispatch(tuple)
+def as_tuple(x):
+    """Get `x` as a tuple. Will be wrapped in a one-tuple if it is not a tuple.
+
+    Args:
+        x (object): Object to get as a tuple.
+
+    Returns:
+        tuple: `x` as a tuple.
+    """
+    return x
+
+
+@_dispatch(object)
+def as_tuple(x):
+    return (x,)
 
 
 def autograd_register(f, s_f):
@@ -24,7 +44,8 @@ def autograd_register(f, s_f):
     # Register the sensitivity.
     def vjp_argnums(nums, y, args, kw_args):
         def vjp(s_y):
-            return tuple(np.take(s_f(s_y, y, *args, **kw_args), nums))
+            grads = as_tuple(s_f(s_y, y, *args, **kw_args))
+            return tuple([grads[i] for i in nums])
 
         return vjp
 