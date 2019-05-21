aa6d0f2e11f3eba67e9cf1d8512ba287e7c76cf0
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-05-21 21:16:22 +0100

    Dimension is a Number

--- lab/types.py
+++ lab/types.py
@@ -91,18 +91,25 @@ _torch_dtype = ModuleType('torch', 'dtype')
 _torch_retrievables = [_torch_tensor,
                        _torch_dtype]
 
-# Numeric types:
-
+# Basic numeric types:
 Int = Union(*([int] + np.sctypes['int'] + np.sctypes['uint']), alias='Int')
 Float = Union(*([float] + np.sctypes['float']), alias='Float')
 Bool = Union(bool, np.bool_, alias='Bool')
-Number = Union(Int, Float, alias='Number')
+
+# Dimension types:
+NPDimension = Union(Int, alias='NPDimension')
+TFDimension = Union(NPDimension, _tf_dimension, alias='TFDimension')
+TorchDimension = Union(NPDimension, alias='TorchDimension')
+Dimension = Union(NPDimension, TFDimension, TorchDimension, alias='Dimension')
+
+# Advanced numeric types:
+Number = Union(Dimension, Int, Float, alias='Number')
 NPNumeric = Union(Number, Bool, np.ndarray, Box, alias='NPNumeric')
 TFNumeric = Union(_tf_tensor, _tf_variable, _tf_refvariable, alias='TFNumeric')
 TorchNumeric = Union(_torch_tensor, alias='TorchNumeric')
 Numeric = Union(NPNumeric, TFNumeric, TorchNumeric, alias='Numeric')
 
-# Define promotion rules and the corresponding conversion methods.
+# Define corresponding promotion rules and conversion methods.
 add_promotion_rule(NPNumeric, TFNumeric, TFNumeric)
 add_promotion_rule(NPNumeric, TorchNumeric, TorchNumeric)
 add_promotion_rule(_tf_tensor, _tf_variable, TFNumeric)
@@ -111,18 +118,10 @@ add_conversion_method(NPNumeric, TFNumeric,
 add_conversion_method(NPNumeric, TorchNumeric,
                       lambda x: _module_call('torch', 'tensor', x))
 
-# Dimension types:
-
-NPDimension = Union(Int, alias='NPDimension')
-TFDimension = Union(NPDimension, _tf_dimension, alias='TFDimension')
-TorchDimension = Union(NPDimension, alias='TorchDimension')
-Dimension = Union(NPDimension, TFDimension, TorchDimension, alias='Dimension')
-
 # Define corresponding conversion methods.
 add_conversion_method(_tf_dimension, int, lambda x: x.value)
 
 # Data types:
-
 NPDType = Union(type, np.dtype, alias='NPDType')
 TFDType = Union(_tf_dtype, alias='TFDType')
 TorchDType = Union(_torch_dtype, alias='TorchDType')
@@ -173,9 +172,6 @@ add_conversion_method(TFDType, TorchDType,
                       lambda x: _module_attr('torch',
                                              _name(x.as_numpy_dtype)))
 
-# # NumPy data types can be of type `np.dtype`. Convert to the proper types.
-# add_conversion_method(np.dtype, DType, lambda x: x.type)
-
 default_dtype = np.float64  #: Default dtype.
 
 
@@ -212,7 +208,6 @@ def dtype(a):
 
 
 # Framework types:
-
 NP = Union(NPNumeric, NPDimension, NPDType, alias='NP')
 TF = Union(TFNumeric, TFDimension, TFDType, alias='TF')
 Torch = Union(TorchNumeric, TorchDimension, TorchDType, alias='Torch')
