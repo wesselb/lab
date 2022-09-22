Fix broadcasting of `randgamma`

lab/tensorflow/random.py
@@ -1,12 +1,11 @@
 import logging
 
 import tensorflow as tf
-from plum import Union
 
 from . import dispatch, B, Numeric
-from ..types import TFDType, TFNumeric, Int, TFRandomState
-from ..util import compress_batch
 from ..random import _randcat_last_first
+from ..types import TFDType, TFNumeric, Int, TFRandomState
+from ..util import compress_batch, broadcast_shapes
 
 __all__ = []
 
@@ -123,13 +122,13 @@ def randgamma(
     alpha: Numeric,
     scale: Numeric,
 ):
-    return state, tf.random.stateless_gamma(
-        shape,
+    sample = tf.random.stateless_gamma(
+        shape + broadcast_shapes(B.shape(alpha), B.shape(scale)),
         alpha=alpha,
-        beta=B.divide(1, scale),
         seed=state.make_seeds()[:, 0],
         dtype=dtype,
     )
+    return state, sample * B.to_active_device(B.cast(dtype, scale))
 
 
 @dispatch

lab/torch/random.py
@@ -1,10 +1,9 @@
 import torch
-from plum import Union
 
 from . import dispatch, B, Numeric
+from ..random import _randcat_last_first
 from ..types import TorchNumeric, TorchDType, Int, TorchRandomState
 from ..util import compress_batch
-from ..random import _randcat_last_first
 
 __all__ = []
 
@@ -137,7 +136,9 @@ def randgamma(
 ):
     alpha = B.to_active_device(B.cast(dtype, alpha))
     scale = B.to_active_device(B.cast(dtype, scale))
-    alpha = B.broadcast_to(alpha, *shape)
+    alpha, scale = torch.broadcast_tensors(alpha, scale)
+    alpha = B.repeat(alpha, *shape)
+    scale = B.repeat(scale, *shape)
     return state, torch._standard_gamma(alpha, generator=state) * scale
 
 